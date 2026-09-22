1bde4dbca0f49bc831269bdc6701719872087f90
Janine <carigjanine55@gmail.com>
2026-09-22 21:32:04 +0800

    git status

---
+++ src/firstJava.java
@@ -0,0 +1,11 @@
+public class firstJava {
+}
+    public static void main (String[] args){
+        String firstname = "janine ";
+        String middlename = "siador ";
+        String lastname = "carig";
+        String section = "1A";
+
+            System.out.println (firstname + middlename + lastname);
+            System.out.println (section);
+    }
---
+++ src/secondJava.java
@@ -0,0 +1,15 @@
+public class secondJava {
+}
+    public static void main (String[] args){
+        String fullname = "Janine S. Carig";
+        String Address ="Mangcasuy, Binalonan, Pangasina";
+        String contactnumber = "09703837087";
+        int age = 19;
+        String crush = "Nickaerah Jill";
+
+        System.out.println ("Full Name: " + fullname);
+        System.out.println ("Address: " + Address);
+        System.out.println ("Contact Number: " + contactnumber);
+        System.out.println ("Age: " + age);
+        System.out.println ("Crush sa klase: " + crush);
+    }
